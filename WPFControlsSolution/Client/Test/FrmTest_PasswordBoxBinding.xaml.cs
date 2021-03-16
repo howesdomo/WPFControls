dc9fb7cebd75e596a4cbfe9ffad893b2c066c934
HowesDOMO <164572429@qq.com>
2021-03-16 17:07:08 +0800

    优化整理 PasswordBox 的代码

--- WPFControlsSolution/Client/Test/FrmTest_PasswordBoxBinding.xaml.cs
+++ WPFControlsSolution/Client/Test/FrmTest_PasswordBoxBinding.xaml.cs
@@ -32,7 +32,7 @@ namespace Client.Test
             {
                 if (objWindow is FrmTest_PasswordBoxBinding owner)
                 {
-                    string msg = $"P1:{this.P1}\r\nP2:{this.P2}";
+                    string msg = $"P1:{this.P1}\r\nP2:{this.P2}\r\nP3:{this.P3}\r\nP4:{this.P4}";
                     MessageBox.Show(owner, msg);
                 }
             });
@@ -50,7 +50,7 @@ namespace Client.Test
         }
 
 
-        private string _P2;
+        private string _P2 = "I'm P2.";
         public string P2
         {
             get { return _P2; }
@@ -61,94 +61,31 @@ namespace Client.Test
             }
         }
 
+        private string _P3;
+        public string P3
+        {
+            get { return _P3; }
+            set
+            {
+                _P3 = value;
+                this.OnPropertyChanged("P3");
+            }
+        }
+
+
+        private string _P4 = "P4 is me";
+        public string P4
+        {
+            get { return _P4; }
+            set
+            {
+                _P4 = value;
+                this.OnPropertyChanged("P4");
+            }
+        }
+
+
         public Command CMD_Show { get; private set; }
 
     }
 }
-
-//namespace Client.ComponentsHelper
-//{
-//    /// <summary>
-//    /// 为PasswordBox控件的Password增加绑定功能
-//    /// </summary>
-//    public static class PasswordBoxHelper
-//    {
-//        public static readonly DependencyProperty PasswordProperty =
-//            DependencyProperty.RegisterAttached("Password",
-//            typeof(string), typeof(PasswordBoxHelper),
-//            new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
-
-//        public static readonly DependencyProperty AttachProperty =
-//            DependencyProperty.RegisterAttached("Attach",
-//            typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, Attach));
-
-//        private static readonly DependencyProperty IsUpdatingProperty =
-//           DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
-//           typeof(PasswordBoxHelper));
-
-//        public static void SetAttach(DependencyObject dp, bool value)
-//        {
-//            dp.SetValue(AttachProperty, value);
-//        }
-
-//        public static bool GetAttach(DependencyObject dp)
-//        {
-//            return (bool)dp.GetValue(AttachProperty);
-//        }
-
-//        public static string GetPassword(DependencyObject dp)
-//        {
-//            return (string)dp.GetValue(PasswordProperty);
-//        }
-
-//        public static void SetPassword(DependencyObject dp, string value)
-//        {
-//            dp.SetValue(PasswordProperty, value);
-//        }
-
-//        private static bool GetIsUpdating(DependencyObject dp)
-//        {
-//            return (bool)dp.GetValue(IsUpdatingProperty);
-//        }
-
-//        private static void SetIsUpdating(DependencyObject dp, bool value)
-//        {
-//            dp.SetValue(IsUpdatingProperty, value);
-//        }
-
-//        private static void OnPasswordPropertyChanged(DependencyObject sender,
-//            DependencyPropertyChangedEventArgs e)
-//        {
-//            PasswordBox passwordBox = sender as PasswordBox;
-//            passwordBox.PasswordChanged -= PasswordChanged;
-//            if (!(bool)GetIsUpdating(passwordBox))
-//            {
-//                passwordBox.Password = (string)e.NewValue;
-//            }
-//            passwordBox.PasswordChanged += PasswordChanged;
-//        }
-
-//        private static void Attach(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-//        {
-//            if (sender is PasswordBox passwordBox)
-//            {
-//                if ((bool)e.OldValue)
-//                {
-//                    passwordBox.PasswordChanged -= PasswordChanged;
-//                }
-//                if ((bool)e.NewValue)
-//                {
-//                    passwordBox.PasswordChanged += PasswordChanged;
-//                }
-//            }
-//        }
-
-//        private static void PasswordChanged(object sender, RoutedEventArgs e)
-//        {
-//            PasswordBox passwordBox = sender as PasswordBox;
-//            SetIsUpdating(passwordBox, true);
-//            SetPassword(passwordBox, passwordBox.Password);
-//            SetIsUpdating(passwordBox, false);
-//        }
-//    }
-//}
---
+++ WPFControlsSolution/WPFControls/PasswordBox/PasswordBoxHelper.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.Components
+{
+    /// <summary>
+    /// 用附加属性的方式, 使 PasswordBox 能在 MVVM 中绑定 string
+    /// 
+    /// V 1.0.0 - 2021-03-16 17:05:40
+    /// 首次创建
+    /// </summary>
+    public static class PasswordBoxHelper
+    {
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetIsUpdating(passwordBox, false);
+        }
+
+        #region [DPA] Password
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached
+            (
+                name: "Password",
+                propertyType: typeof(string),
+                ownerType: typeof(PasswordBoxHelper),
+                defaultMetadata: new FrameworkPropertyMetadata
+                (
+                    defaultValue: string.Empty,
+                    propertyChangedCallback: onHandle_Password_PropertyChanged
+                )
+            );
+
+        public static string GetPassword(DependencyObject dp)
+        {
+            return (string)dp.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject dp, string value)
+        {
+            dp.SetValue(PasswordProperty, value);
+        }
+
+
+        private static void onHandle_Password_PropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            passwordBox.PasswordChanged -= PasswordChanged;
+            if (!(bool)GetIsUpdating(passwordBox)) // IsUpdating -- 正在输入, 若果正在输入中, 不会更新实际 PasswordBox 的值
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+
+        #endregion
+
+
+        #region MyRegion        
+
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached
+            (
+                name: "Attach",
+                propertyType: typeof(bool),
+                ownerType: typeof(PasswordBoxHelper),
+                defaultMetadata: new FrameworkPropertyMetadata
+                (
+                    defaultValue: false,
+                    propertyChangedCallback: onHandle_Attach_PropertyChanged
+                )
+            );
+
+
+        public static void SetAttach(DependencyObject dp, bool value)
+        {
+            dp.SetValue(AttachProperty, value);
+        }
+
+        public static bool GetAttach(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(AttachProperty);
+        }
+
+        private static void onHandle_Attach_PropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                if ((bool)e.OldValue)
+                {
+                    passwordBox.PasswordChanged -= PasswordChanged;
+                }
+                if ((bool)e.NewValue)
+                {
+                    passwordBox.PasswordChanged += PasswordChanged;
+                }
+            }
+        }
+
+        #endregion
+
+        #region [DPA] IsUpdating 
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+            DependencyProperty.RegisterAttached
+            (
+                name: "IsUpdating",
+                propertyType: typeof(bool),
+                ownerType: typeof(PasswordBoxHelper)
+            );
+
+        private static bool GetIsUpdating(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+        {
+            dp.SetValue(IsUpdatingProperty, value);
+        }
+
+        #endregion
+
+    }
+}
