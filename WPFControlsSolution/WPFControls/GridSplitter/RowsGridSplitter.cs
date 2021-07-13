9e31acaa446b4f8b5c21051eb8de940f981d6203
HowesDOMO <164572429@qq.com>
2021-07-13 16:45:11 +0800

    RowsGridSplitter 采用 C#代码编写 ControlTemplate 的方式实现

--- WPFControlsSolution/WPFControls/GridSplitter/RowsGridSplitter.cs
+++ WPFControlsSolution/WPFControls/GridSplitter/RowsGridSplitter.cs
@@ -4,123 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace Client.Components
 {
-    //    public class RowsGridSplitter : GridSplitter
-    //    {
-    //        public RowsGridSplitter()
-    //        {
-    //            Width = double.NaN;
-    //            Height = 14;
-    //            Background = System.Windows.Media.Brushes.WhiteSmoke;
-    //            HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
-    //            ResizeBehavior = GridResizeBehavior.PreviousAndNext;
-    //            ResizeDirection = GridResizeDirection.Rows;
-    //            ShowsPreview = false;
-
-    //            //try
-    //            //{
-    //            //    this.Template = (ControlTemplate)Resources["ControlTemplate_RowsGridSplitter"];
-    //            //}
-    //            //catch (Exception ex)
-    //            //{
-    //            //    string msg = $"{0}";
-    //            //    System.Diagnostics.Debug.WriteLine(msg);
-    //            //}
-
-
-    //            var template = new ControlTemplate(typeof(RowsGridSplitter)) { };
-
-    //            var element = new System.Windows.FrameworkElementFactory(typeof(Grid));
-    //            // element.AppendChild()
-
-
-    //            StackPanel p = new StackPanel()
-    //            {
-    //                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
-    //                Orientation = Orientation.Horizontal
-    //            };
-
-    //            p.Children.Add(AAAA());
-    //            p.Children.Add(AAAA());
-    //            p.Children.Add(AAAA());
-
-
-
-    //            //element.SetValue(TextBlock.PaddingProperty, new System.Windows.Thickness(0, 0, 0, 0));
-    //            //element.SetValue(TextBlock.HorizontalAlignmentProperty, System.Windows.HorizontalAlignment.Stretch);
-    //            //element.SetValue(TextBlock.VerticalAlignmentProperty, System.Windows.VerticalAlignment.Top);
-    //            //element.SetValue(TextBlock.TextProperty, "...");
-    //            //element.SetValue(TextBlock.BackgroundProperty, System.Windows.Media.Brushes.Orange);
-    //            // element.SetValue(TextBlock.TextAlignmentProperty, System.Windows.TextAlignment.Center);
-
-    //            template.VisualTree = element;
-
-    //            this.Template = template;
-
-    //            // test2();
-    //        }
-
-    //        System.Windows.Shapes.Ellipse AAAA()
-    //        {
-    //            return new System.Windows.Shapes.Ellipse()
-    //            {
-    //                Width = 2,
-    //                Height = 2,
-    //                Margin = new System.Windows.Thickness(1,1,1,1),
-    //                Fill = System.Windows.Media.Brushes.Black
-    //            };
-    //        }
-
-
-    //        void test2()
-    //        {
-    //            // TODO 读取 XAML 方式加载 Template
-
-    //            string xaml =
-    //"<ControlTemplate>" +
-    //    "<Grid>" +
-    //        "<TextBlock " +
-    //        "    Padding=\"0,-8,0,0\"" +
-    //        "    HorizontalAlignment=\"Stretch\"" +
-    //        "    Text=\"...\"" +
-    //        "    TextAlignment=\"Center\" />" +
-    //    "</Grid>" +
-    //"</ControlTemplate>";
-
-    //            this.Template = (ControlTemplate)System.Windows.Markup.XamlReader.Parse(xaml);
-    //        }
-    //    }
-
-
-
-
-    #region 以 CustomControl 方式编码
-
-
+    #region [不推荐使用, 但需要了解] 在 C# 中编写 ControlTemplate
+    
     public class RowsGridSplitter : GridSplitter
     {
-        static RowsGridSplitter()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(RowsGridSplitter), new FrameworkPropertyMetadata(typeof(RowsGridSplitter)));
-        }
-
         public RowsGridSplitter()
         {
             Width = double.NaN;
-            Height = 6;
-            HorizontalAlignment = HorizontalAlignment.Stretch;
 
-            // Panel.Zin
-
+            Background = System.Windows.Media.Brushes.WhiteSmoke;
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
             ResizeBehavior = GridResizeBehavior.PreviousAndNext;
             ResizeDirection = GridResizeDirection.Rows;
             ShowsPreview = false;
-        }
 
+            #region ControlTemplate
+
+            // 用 C# 代码的方式, 实现以下的 XAML 代码
+            //  1 //<ControlTemplate TargetType="{x:Type local:RowsGridSplitter}">
+            //  2 //    <Grid Background="Transparent">
+            //  3 //        <StackPanel
+            //  4 //            HorizontalAlignment="Center"
+            //  5 //            Orientation="Horizontal">
+            //  6 //            <Ellipse Width="2" Height="2" Margin="1" Fill="Gray" />
+            //  7 //            <Ellipse Width="2" Height="2" Margin="1" Fill="Gray" />
+            //  8 //            <Ellipse Width="2" Height="2" Margin="1" Fill="Gray" />
+            //  9 //        </StackPanel>
+            // 10 //    </Grid>
+            // 11 //</ControlTemplate>
+
+            var controlTemplate = new ControlTemplate(typeof(RowsGridSplitter));
+
+            var gridFactory = new FrameworkElementFactory(typeof(Grid));
+
+            gridFactory.SetValue(Grid.BackgroundProperty, System.Windows.Media.Brushes.Transparent);
+
+            var stackPanelFactory = new FrameworkElementFactory(typeof(StackPanel));
+
+            stackPanelFactory.SetValue(StackPanel.HorizontalAlignmentProperty, System.Windows.HorizontalAlignment.Center);
+            stackPanelFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+
+            for (int i = 0; i < 3; i++)
+            {
+                var ellipse = new FrameworkElementFactory(typeof(Ellipse));
+                ellipse.SetValue(Ellipse.WidthProperty, 2d);
+                ellipse.SetValue(Ellipse.HeightProperty, 2d);
+                ellipse.SetValue(Ellipse.MarginProperty, new System.Windows.Thickness(1, 1, 1, 1));
+                ellipse.SetValue(Ellipse.FillProperty, System.Windows.Media.Brushes.Gray);
+
+                stackPanelFactory.AppendChild(ellipse);
+            }
+
+            gridFactory.AppendChild(stackPanelFactory);
+
+            controlTemplate.VisualTree = gridFactory;
+
+            #endregion
+
+            this.Template = controlTemplate;
+        }
     }
 
     #endregion
 
+    #region [推荐使用] 以 CustomControl 方式编码
+
+    //public class RowsGridSplitter : GridSplitter
+    //{
+    //    static RowsGridSplitter()
+    //    {
+    //        DefaultStyleKeyProperty.OverrideMetadata(typeof(RowsGridSplitter), new FrameworkPropertyMetadata(typeof(RowsGridSplitter)));
+    //    }
+
+    //    public RowsGridSplitter()
+    //    {
+    //        Width = double.NaN;
+    //        Height = 6;
+    //        HorizontalAlignment = HorizontalAlignment.Stretch;
+
+    //        // Panel.Zin
+
+    //        ResizeBehavior = GridResizeBehavior.PreviousAndNext;
+    //        ResizeDirection = GridResizeDirection.Rows;
+    //        ShowsPreview = false;
+    //    }
+
+    //}
+
+    #endregion
 }
