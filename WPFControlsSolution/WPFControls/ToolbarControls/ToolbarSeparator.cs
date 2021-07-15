6b79b6747be552ddf4fa3cc90e3c590a8aad466e
HowesDOMO <164572429@qq.com>
2021-07-15 11:10:13 +0800

    新增 ToolbarSeparator

---
+++ WPFControlsSolution/WPFControls/ToolbarControls/ToolbarSeparator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+
+namespace Client.Components.ToolbarControls
+{
+   public class ToolbarSeparator : System.Windows.Controls.UserControl
+    {
+        public ToolbarSeparator()
+        {
+            var r = new Rectangle();
+            r.Width = 1d;
+            // a.Height = Auto
+
+            r.Margin = new System.Windows.Thickness(3, 7, 3, 7);
+            r.Fill = System.Windows.Media.Brushes.Black;
+
+            this.Content = r;
+        }
+    }
+}
